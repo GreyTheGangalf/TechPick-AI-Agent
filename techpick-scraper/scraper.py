f57b07d783d8b04591ba5c070c7fb6f02c1d1b0a
GreyTheGangalf <erkinarikan38@gmail.com>
2026-06-30 14:20:29 +0300

    feat(scraper): First successful data retrieval from epey.com (BeautifulSoup)

--- techpick-scraper/scraper.py
+++ techpick-scraper/scraper.py
@@ -8,23 +8,41 @@ HEADERS = {
 }
 
 def scrape_laptops():
-    print("Connecting to website...")
+    print("Connecting to the target site...")
+    response = requests.get(TARGET_URL, headers=HEADERS)
+    
+    if response.status_code == 200:
+        print("Connection successful! Scanning products...\n")
+        soup = BeautifulSoup(response.content, "html.parser")
+        
+        products = soup.find_all("ul", class_="metin row")
 
-    try:
-        response = requests.get(TARGET_URL, headers=HEADERS)
+        print(f"A total of {len(products)} product cards were found.\n")
+        print("=" * 40)
+        
+        for item in products:
+            try:
+                name_tag = item.find("a", class_="urunadi")
+                laptop_name = name_tag.text.strip() if name_tag else "Name Not Found"
+                
 
-        print(f"Connection status: {response.status_code}")
+                price_tag = item.find("li", class_="fiyat")
+                
+                if price_tag:
 
-        if response.status_code == 200:
-            print("Connection successful, Data has been collected!")
-
-            soup = BeautifulSoup(response.content, "html.parser")
-
-            print(f"Page Header: {soup.title.text.strip()}")
-        else:
-            print("Connection Error")
-    except Exception as e:
-        print(f"An error occured: {e}")
+                    raw_price = price_tag.text.strip().split('\n')[0]
+                else:
+                    raw_price = "No Price"
+                
+                if laptop_name != "Name Not Found":
+                    print(f"Model: {laptop_name}")
+                    print(f"Price: {raw_price}")
+                    print("-" * 40)
+                    
+            except Exception as e:
+                continue
+    else:
+        print(f"Connection Error! Status: {response.status_code}")
 
 if __name__ == "__main__":
     scrape_laptops()
